b5b56deea56390b98e95e6a99d2975f69a4f836f
Evgeny Morash <evgenymorash@gmail.com>
2022-08-01 10:58:54 +0300

    add: increase action creator, first part of reducer

--- src/redux/state-reducer.ts
+++ src/redux/state-reducer.ts
@@ -1,3 +1,32 @@
-export const counterReducer = () => {
+
+
+export const INC_COUNT = "INC-COUNT"
+
+
+export type ActionType = IncCountAT
+
+export type IncCountAT = ReturnType<typeof incCountAC>
+
+
+const initialState = {
+    count: 10
+}
+
+export type StateType = typeof initialState
+
+export const counterReducer = (state: StateType = initialState, action: ActionType) => {
+    switch (action.type) {
+        case INC_COUNT:
+            return {...state, count: action.count + 1}
+        default:
+            return state
+    }
 
 }
+
+
+
+export const incCountAC = (count: number) => ({
+    type: INC_COUNT,
+    count
+}) as const
