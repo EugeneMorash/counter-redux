9d31fe75a28fa602fd1a6de91cbdf2494f21a6d1
Evgeny Morash <evgenymorash@gmail.com>
2022-08-01 11:19:54 +0300

    add: primary layout, material UI stylization

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
 import './App.css';
+import {Button, Container, makeStyles, Paper} from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+    root: {
+        flexGrow: 1
+    },
+    main: {},
+    paperStyle: {
+        maxWidth: 400,
+        margin: 20,
+        padding: 20,
+        backgroundColor: "gray",
+        fontSize: 30
+    },
+    button: {
+        backgroundColor: "gold",
+        border: '2px solid green',
+        borderRadius: 10
+    }
+}))
 
 function App() {
+    const classes = useStyles()
   return (
-    <div>
-      111
-    </div>
+      <div style={{backgroundColor: 'lightblue'}}>
+          <Container fixed>
+              <main className={classes.main}>
+                  <Paper className={classes.paperStyle}>
+                      Max value: <input type="number"/>
+                      <br/>
+                      Start value: <input type="number"/>
+                      <br/>
+                      <Button variant="outlined" className={classes.button}>Set</Button>
+                  </Paper>
+                  <Paper className={classes.paperStyle}>
+                      222
+                      <br/>
+                      <Button variant="outlined" className={classes.button}>Inc</Button>
+                      <Button variant="outlined" className={classes.button}>Reset</Button>
+                  </Paper>
+
+              </main>
+          </Container>
+
+      </div>
   );
 }
 
